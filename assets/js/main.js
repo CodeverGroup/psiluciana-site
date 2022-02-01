e4aadd5dbd77b98678cda18f3052f57ecd59d618
Everton Gonçalves <dsgeverton@gmail.com>
2022-02-01 10:26:25 -0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,28 @@
 const btn = document.getElementById("btn-mobile")
+const menuBtn1 = document.querySelector('.menu-buttom-1')
+const menuBtn2 = document.querySelector('.menu-buttom-2')
+const menuBtn3 = document.querySelector('.menu-buttom-3')
+
+menuBtn1.addEventListener('click', () => {
+    const nav = document.getElementById("nav")
+    if (nav.className.match('active')) {
+        nav.classList.remove('active')
+    }
+})
+
+menuBtn2.addEventListener('click', () => {
+    const nav = document.getElementById("nav")
+    if (nav.className.match('active')) {
+        nav.classList.remove('active')
+    }
+})
+
+menuBtn3.addEventListener('click', () => {
+    const nav = document.getElementById("nav")
+    if (nav.className.match('active')) {
+        nav.classList.remove('active')
+    }
+})
 
 btn.addEventListener('click', () => {
     const nav = document.getElementById("nav")
