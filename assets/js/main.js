883735cb005335f5dde52f3eb689ba9c0258f175
Everton Gonçalves <dsgeverton@gmail.com>
2022-02-01 15:45:14 -0300

    re-included contact fields

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 const btn = document.getElementById("btn-mobile")
 const menuBtn1 = document.querySelector('.menu-buttom-1')
-// const menuBtn2 = document.querySelector('.menu-buttom-2')
+const menuBtn2 = document.querySelector('.menu-buttom-2')
 const menuBtn3 = document.querySelector('.menu-buttom-3')
 
 menuBtn1.addEventListener('click', () => {
@@ -10,12 +10,12 @@ menuBtn1.addEventListener('click', () => {
     }
 })
 
-// menuBtn2.addEventListener('click', () => {
-//     const nav = document.getElementById("nav")
-//     if (nav.className.match('active')) {
-//         nav.classList.remove('active')
-//     }
-// })
+menuBtn2.addEventListener('click', () => {
+    const nav = document.getElementById("nav")
+    if (nav.className.match('active')) {
+        nav.classList.remove('active')
+    }
+})
 
 menuBtn3.addEventListener('click', () => {
     const nav = document.getElementById("nav")
